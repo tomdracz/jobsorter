ce3df91413cf3ae2aaf711507c1427ff7665fd93
Tom Dracz <tom.dracz@findsyou.com>
2016-06-21 15:43:13 +0100

    Add self dependency error check

--- lib/jobs_sorter.rb
+++ lib/jobs_sorter.rb
@@ -1,4 +1,7 @@
 class JobsSorter
+  class SelfDependencyError < StandardError
+  end
+
   attr_reader :results
 
   def initialize(jobs)
@@ -8,6 +11,15 @@ class JobsSorter
 
   def parse(job_string)
     jobs_hash = job_string.scan(/([a-z]) => ?([a-z]?)/).to_h
+    has_self_dependency?(jobs_hash)
     return jobs_hash
+  end
+
+  private
+
+  def has_self_dependency?(jobs_hash)
+    jobs_hash.each do |k,v|
+      raise SelfDependencyError, "self dependency detected" if k == v
+    end
   end
 end
--- spec/jobs_sorter_spec.rb
+++ spec/jobs_sorter_spec.rb
@@ -10,4 +10,14 @@ describe JobsSorter do
       expect(jobs_sorter.parse(jobs)).to eql({"a" => "", "b" => "", "c" => ""})
     end
   end
+
+  describe "dependency errors" do
+    context "when given a string with self dependency error" do
+      let(:jobs) { "a => b c => c"}
+
+      it "raises a self dependency error" do
+        expect{jobs_sorter}.to raise_error(JobsSorter::SelfDependencyError)
+      end
+    end
+  end
 end
