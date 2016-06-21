a7d6365ea66aa836197d1cf02326dad91158e0fa
Tom Dracz <tom.dracz@findsyou.com>
2016-06-21 15:32:04 +0100

    Add method to convert jobs string into a hash

--- lib/jobs_sorter.rb
+++ lib/jobs_sorter.rb
@@ -1,2 +1,13 @@
 class JobsSorter
+  attr_reader :results
+
+  def initialize(jobs)
+    @jobs_hash = parse(jobs)
+    @results = []
+  end
+
+  def parse(job_string)
+    jobs_hash = job_string.scan(/([a-z]) => ?([a-z]?)/).to_h
+    return jobs_hash
+  end
 end
--- spec/jobs_sorter_spec.rb
+++ spec/jobs_sorter_spec.rb
@@ -1,5 +1,13 @@
 require 'jobs_sorter'
 
 describe JobsSorter do
+  let(:jobs_sorter) { JobsSorter.new(jobs) }
 
+  describe "#parse" do
+    let(:jobs) { "a =>  b =>  c =>" }
+
+    it "converts jobs string into a hash" do
+      expect(jobs_sorter.parse(jobs)).to eql({"a" => "", "b" => "", "c" => ""})
+    end
+  end
 end
